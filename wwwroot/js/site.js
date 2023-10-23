06a03bb172c8a22850f2bac4f44cd9317e90f101
paulotxt <101910150+paulotxt@users.noreply.github.com>
2023-10-22 21:51:10 -0300

    Add files via upload

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function validarFormulario() {
+    var titulo = document.getElementById("titulo").value;
+    var autor = document.getElementById("autor").value;
+    var ano = document.getElementById("ano").value;
+
+    if (titulo === "" || autor === "" || ano === "") {
+        alert("Por favor, preencha todos os campos.");
+        return false;
+    }
+
+    if (isNaN(ano) || ano < 0) {
+        alert("Ano de publicação inválido.");
+        return false;
+    }
+
+    return true;
+}
